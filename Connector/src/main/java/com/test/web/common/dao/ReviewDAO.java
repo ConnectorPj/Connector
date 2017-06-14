d7dc7d311527d2df646445a496f1cd759e549811
YOON\admin <03heiya27@gmail.com>
2017-06-14 15:40:21 +0900

    리뷰다오 재업로드

---
+++ Connector/src/main/java/com/test/web/common/dao/ReviewDAO.java
@@ -0,0 +1,18 @@
+package com.test.web.common.dao;
+
+import java.util.List;
+import com.test.web.common.bean.ReviewBean;
+
+public interface ReviewDAO {
+	public ReviewBean selectReview(ReviewBean bean);
+	
+	public List<ReviewBean> selectReviewListAll(ReviewBean bean);
+	
+	public List<ReviewBean> selectReviewList(ReviewBean bean);
+	
+	public int insertReview(ReviewBean bean);
+	
+	public int updateReview(ReviewBean bean);
+	
+	public int deleteReview(ReviewBean bean);
+}
