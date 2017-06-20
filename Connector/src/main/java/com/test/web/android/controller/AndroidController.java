f2a313faf17715cf06a2db5116daae98aaa7340a
HONG-UI\HongUi <1993jhy@gmail.com>
2017-06-20 18:14:30 +0900

    안드로이드용 컨트롤러 내용 수정

--- Connector/src/main/java/com/test/web/android/controller/AndroidController.java
+++ Connector/src/main/java/com/test/web/android/controller/AndroidController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.test.web.common.bean.CustomerBean;
+import com.test.web.common.bean.PhotoBean;
+import com.test.web.common.bean.TeacherBean;
 import com.test.web.common.dao.ClassDAO;
 import com.test.web.common.dao.CustomerDAO;
 import com.test.web.common.dao.PhotoDAO;
@@ -25,43 +27,59 @@ import com.test.web.common.dao.TeacherDAO;
 @Controller
 public class AndroidController {
 
-	// 파일 업로드 저장경로
-	@Value("#{config['file.upload.path']}")
-	private String FILE_UPLOAD_PATH;
+   // 파일 업로드 저장경로
+   @Value("#{config['file.upload.path']}")
+   private String FILE_UPLOAD_PATH;
 
-	@Autowired
-	private CustomerDAO customerDAO;
-	
-	@Autowired
-	private PurchaseDAO purchaseDAO;
-	
-	@Autowired
-	private TeacherDAO teacherDao;
+   @Autowired
+   private CustomerDAO customerDAO;
+   
+   @Autowired
+   private PurchaseDAO purchaseDAO;
+   
+   @Autowired
+   private TeacherDAO teacherDAO;
 
-	@Autowired
-	private PhotoDAO photoDao;
+   @Autowired
+   private PhotoDAO photoDAO;
 
-	@Autowired
-	private ClassDAO classDao;
+   @Autowired
+   private ClassDAO classDAO;
 
-	@RequestMapping("/androidProfile")
-	@ResponseBody
-	public Map<String, Object> androidProfile(HttpServletRequest request) {
-		
-		System.out.println(request.getParameter("user"));
-		Gson gson = new Gson();
-		CustomerBean customerBean = gson.fromJson(request.getParameter("user"), CustomerBean.class);
-		System.out.println(customerBean.getCustomerId());
-		
-		customerBean = customerDAO.selectCustomer(customerBean);
-		
-		Map<String, Object> result = new HashMap<>();
-		result.put("customerName",customerBean.getCustomerName());
-		result.put("customerCellphone",customerBean.getCustomerCellphone());
-		result.put("customerProfile",customerBean.getCustomerProfile());
-		result.put("customerGender",customerBean.getCustomerGender());
-		
-		return result;
-	}
-	
+   @RequestMapping("/androidProfile")
+   @ResponseBody
+   public Map<String, Object> androidProfile(HttpServletRequest request) {
+      
+      System.out.println(request.getParameter("user"));
+      CustomerBean customerBean = new CustomerBean();
+      PhotoBean photoBean = new PhotoBean();
+      customerBean.setCustomerId(request.getParameter("userId"));
+      photoBean.setMemberId(request.getParameter("userId"));
+      System.out.println(request.getParameter("userId"));
+      
+      photoBean = photoDAO.selectPhoto(photoBean);
+      customerBean = customerDAO.selectCustomer(customerBean);
+      Map<String, Object> result = new HashMap<>();
+      if(customerBean == null) {
+         TeacherBean teacherBean = new TeacherBean();
+         teacherBean.setTeacherId(request.getParameter("userId"));
+         teacherBean = teacherDAO.selectTeacher(teacherBean);
+         
+         result.put("userName",teacherBean.getTeacherName());
+         result.put("userCellphone",teacherBean.getTeacherCellphone());
+         result.put("userProfile",teacherBean.getTeacherInfo());
+         result.put("userGender",teacherBean.getTeacherGender());
+         
+      }else{
+         result.put("userName",customerBean.getCustomerName());
+         result.put("userCellphone",customerBean.getCustomerCellphone());
+         result.put("userProfile",customerBean.getCustomerProfile());
+         result.put("userGender",customerBean.getCustomerGender());
+      }
+      
+      result.put("photoFileName",photoBean.getPhotoFileName());
+      
+      return result;
+   }
+   
 }
