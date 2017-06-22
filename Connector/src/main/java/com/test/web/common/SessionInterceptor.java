3b1090b269afa5eaaa6877c471f6ba7c6c309b14
YOON\admin <03heiya27@gmail.com>
2017-06-22 18:15:51 +0900

    인터셉더입니당

--- Connector/src/main/java/com/test/web/common/SessionInterceptor.java
+++ Connector/src/main/java/com/test/web/common/SessionInterceptor.java
@@ -1,23 +1,21 @@
-//package com.test.web.common;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-//
-//import com.test.web.common.bean.CustomerBean;
-//
-//public class SessionInterceptor extends HandlerInterceptorAdapter {
-//
-//	@Override
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception 
-//	{
-//		if( request.getSession().getAttribute(Constants.MEMBER_LOGIN_BEAN) != null ) return true;
-//		
-//		response.sendRedirect("/main.do");
-//		
-//		return false;
-//	}
-//	
-//}
+package com.test.web.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception 
+	{
+		if( request.getSession().getAttribute(Constants.MEMBER_LOGIN_BEAN) != null ) return true;
+		
+		response.sendRedirect("/main.do");
+		
+		return false;
+	}
+	
+}
